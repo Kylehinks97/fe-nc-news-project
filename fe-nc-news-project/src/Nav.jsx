adb0b10ab7d231025da4fb80f06635cd6bfc805b
Kylehinks97 <kyle.s.hinks@outlook.com>
2023-05-31 10:53:29 +0100

    import link component

--- fe-nc-news-project/src/Nav.jsx
+++ fe-nc-news-project/src/Nav.jsx
@@ -1,11 +1,11 @@
-
+import {Link} from "react-router-dom"
 
 function Nav() {
 
     return (
         <nav>
         <h1>NC News</h1>
-        <Link><h2>Home</h2></Link>
+        <Link to="/"><h2>Home</h2></Link>
         <h2>Topics</h2>
         </nav>
     )
