e280aea45fbe92b9be8929395ec6099b035642b4
Kylehinks97 <kyle.s.hinks@outlook.com>
2023-05-31 11:33:55 +0100

    able to view single article

--- fe-nc-news-project/src/utils.js
+++ fe-nc-news-project/src/utils.js
@@ -1,9 +1,25 @@
-import axios from "axios"
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://nc-news-project-lsdh.onrender.com/api",
+});
 
 export function fetchArticles() {
-    return axios.get(`https://nc-news-project-lsdh.onrender.com/api/articles/`).then((result) => {
+  return api
+    .get("/articles/")
+    .then((result) => {
+      return result;
+    })
+    .catch((err) => {
+      console.log(err, "request error");
+    });
+}
+
+export function fetchArticlesById(article_id) {
+    return api.get(`/articles/${article_id}`)
+    .then((result) => {
         return result
-    }).catch(() => {
-        console.log("request error")
+    }).catch((err) => {
+        console.log(err, "request error");
     })
 }
