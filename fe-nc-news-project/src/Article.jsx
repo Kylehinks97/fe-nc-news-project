cba5ac0084a7905e5320c53df2a35bb2867fddce
Kylehinks97 <kyle.s.hinks@outlook.com>
2023-05-31 11:48:57 +0100

    add Loading and remove console.logs

--- fe-nc-news-project/src/Article.jsx
+++ fe-nc-news-project/src/Article.jsx
@@ -6,22 +6,27 @@ import { useEffect, useState } from 'react';
 export default function Article() {
     const [article, setArticle] = useState([])
     const {article_id} = useParams()
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
         fetchArticlesById(article_id).then((result) => {
             setArticle(result.data.article)
-            console.log(article);
+            setIsLoading(false)
         })
     }, [article_id])
 
     
     return (
         <>
+         {isLoading ? <p>Loading...</p> :
+         <>
         <h3>{article.title}</h3>
         <h4>by {article.author}</h4>
         <img src={article.article_img_url}></img>
         <p>{article.body}</p>
         <p>Votes: {article.votes}</p>
+        </>
+    }
         </>
     )
 }
