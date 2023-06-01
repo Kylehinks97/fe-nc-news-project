075854f4126f30c213fad147ae3104b351341ba7
Kylehinks97 <kyle.s.hinks@outlook.com>
2023-06-01 08:33:18 +0100

    added no comments case

--- fe-nc-news-project/src/Comments.jsx
+++ fe-nc-news-project/src/Comments.jsx
@@ -1,35 +1,37 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 import { fetchCommentsByArticleId } from "./utils";
 
 export default function Comments() {
+  const [comments, setComments] = useState([]);
+  const { article_id } = useParams();
 
-    const [comments, setComments] = useState([])
-    const {article_id} = useParams()
-    
-
-useEffect(() => {
+  useEffect(() => {
     fetchCommentsByArticleId(article_id).then((result) => {
-        setComments(result.data.comments)
-        console.log(comments);
-    })
-}, [])
+      setComments(result.data.comments);
+    });
+  }, []);
 
-return (<>
-  <h4>Add Comment:</h4>
-  <input></input>
-  <h3>Comments</h3>
-  {comments.map((comment) => {
-    const formattedDate = new Date(comment.created_at).toLocaleString();
-      return <>
-    <h5>{comment.author}</h5>
-    <p>{comment.body}</p>
-    <p>Votes: {comment.votes}</p>
-    <p>Posted at: {formattedDate}</p>
+  return (
+    <>
+      <h4>Add Comment:</h4>
+      <input></input>
+      <h3>Comments</h3>
+      {comments.length === 0 ? (
+        <p>No Comments</p>
+      ) : (
+        comments.map((comment) => {
+          const formattedDate = new Date(comment.created_at).toLocaleString();
+          return (
+            <section key={comment.comment_id}>
+              <h5>{comment.author}</h5>
+              <p>{comment.body}</p>
+              <p>Votes: {comment.votes}</p>
+              <p>Posted at: {formattedDate}</p>
+            </section>
+          );
+        })
+      )}
     </>
-  }
-  )}
-  
-  
-  </>)
+  );
 }
